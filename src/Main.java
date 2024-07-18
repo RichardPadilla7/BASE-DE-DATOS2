b7a42e2e9d33f5ab1538211d7c32f83ca4a999ec
1-21-01-E043-29\Esf0t <Richard>
2024-07-18 08:06:56 -0500

    RICHARD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3306/estudiantes2024A";
+        String user = "root";
+        String password = "123456";
+        Connection conexion = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            //establecer conexion
+            conexion = DriverManager.getConnection(url,user,password);
+            //preparar la sentencia sql
+            String sql = "UPDATE estudiante SET bimestreUno= ? WHERE cedula=?";
+            pstmt = conexion.prepareStatement(sql);
+
+            //seteamos los valores de la sentencia previa
+            pstmt.setInt(1, 7); //en esta linea se hara la modificicacion que se mostrara en sql, tu modificas.
+            pstmt.setString(2, "1736635219"); // aqui es donde tu pondras a quien quieres modificar
+
+            //imprimir el enunciado
+            System.out.println(sql);
+            int result=pstmt.executeUpdate();
+            System.out.println("Se actualizo "+result+" filas");
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                //cerrando la conexion
+                if(pstmt != null){
+                    pstmt.close();
+                }
+                if (conexion != null){
+                    conexion.close();
+                }
+            }catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
